38e424c5eb7364aa67924e47a8d78bf126828fc4
Andrew Shen <39780709+AndrewShen31@users.noreply.github.com>
2018-09-01 20:51:29 -0700

    Centilator 2019 [Indev 2.0]
    Add basic calulator function.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+### Centilator 2019 [Indev 2.0] - 8/31/2018
+* Has basic feutures set!
+* Will move to alpha stage
+
+
 ### Centilator 2019 [Indev 1.0] - 8/31/2018
 * project created
--- src/main.py
+++ src/main.py
@@ -1,19 +1,63 @@
-import pyglet as p
-from pyglet.window import mouse
+from tkinter import *
 
-window = p.window.Window()
+rt = Tk()
+rt.title("Centilator 2019 [Indev 1.1]")
+rt.geometry("500x500")
 
-label = p.text.Label('Hello, world', font_name='Times New Roman', font_size=36, x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center')
+SUP = Frame(rt)
+KPD_SUP = Frame(rt)
+KPD_TOP = Frame(rt)
+KPD_MID = Frame(rt)
+KPD_BOT = Frame(rt)
+KPD_SUB = Frame(rt)
+SUB = Frame(rt, bg="#ffff00")
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
+def calc(equ):
+    return eval(equ)
 
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    print(x, y, button, modifiers)
-    if button == mouse.LEFT:
-        print("The left mouse button was pressed.")
+def get():
+    txt = inp.get()
+    ans = calc(txt)
+    print(ans)
 
-p.app.run()
+def insertKey(key, var):
+    var.insert(END, key)
+
+def clear(var): var.delete(0, END)
+
+def oppose(var):
+    if var.get()[0] == "-": var.delete(0)
+    else: var.insert(0, "-")
+
+inp = Entry(SUP)
+inp.pack()
+
+Button(KPD_SUP, text="A/C", command=lambda: clear(inp)).pack(side=LEFT)
+Button(KPD_SUP, text="+/-", command=lambda: oppose(inp)).pack(side=LEFT)
+Button(KPD_SUP, text="%", command=lambda: insertKey("1", inp)).pack(side=LEFT)
+Button(KPD_SUP, text="/", command=lambda: insertKey("/", inp)).pack(side=LEFT)
+Button(KPD_TOP, text="1", command=lambda: insertKey("1", inp)).pack(side=LEFT)
+Button(KPD_TOP, text="2", command=lambda: insertKey("2", inp)).pack(side=LEFT)
+Button(KPD_TOP, text="3", command=lambda: insertKey("3", inp)).pack(side=LEFT)
+Button(KPD_TOP, text="*", command=lambda: insertKey("*", inp)).pack(side=LEFT)
+Button(KPD_MID, text="4", command=lambda: insertKey("4", inp)).pack(side=LEFT)
+Button(KPD_MID, text="5", command=lambda: insertKey("5", inp)).pack(side=LEFT)
+Button(KPD_MID, text="6", command=lambda: insertKey("6", inp)).pack(side=LEFT)
+Button(KPD_MID, text="-", command=lambda: insertKey("-", inp)).pack(side=LEFT)
+Button(KPD_BOT, text="7", command=lambda: insertKey("7", inp)).pack(side=LEFT)
+Button(KPD_BOT, text="8", command=lambda: insertKey("8", inp)).pack(side=LEFT)
+Button(KPD_BOT, text="9", command=lambda: insertKey("9", inp)).pack(side=LEFT)
+Button(KPD_BOT, text="+", command=lambda: insertKey("+", inp)).pack(side=LEFT)
+Button(KPD_SUB, text="0", command=lambda: insertKey("0", inp)).pack(side=LEFT)
+Button(KPD_SUB, text=".", command=lambda: insertKey(".", inp)).pack(side=LEFT)
+Button(KPD_SUB, text="=", command=lambda: get()).pack(side=LEFT)
+
+SUP.pack()
+KPD_SUP.pack()
+KPD_TOP.pack()
+KPD_MID.pack()
+KPD_BOT.pack()
+KPD_SUB.pack()
+SUB.pack()
+
+rt.mainloop()
